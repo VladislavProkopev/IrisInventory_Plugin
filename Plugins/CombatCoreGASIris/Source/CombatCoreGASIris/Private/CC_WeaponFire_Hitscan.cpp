4006352bafb9d7be83bcdf027f8a45afec0accf5
Vladislav <vladislavprokopevcplusplus@gmail.com>
2026-05-15 15:20:06 +0500

    CC_WeaponFire_Hitscan

---
+++ Plugins/CombatCoreGASIris/Source/CombatCoreGASIris/Private/CC_WeaponFire_Hitscan.cpp
@@ -0,0 +1,121 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CC_WeaponFire_Hitscan.h"
+
+#include "AbilitySystemComponent.h"
+#include "CC_WeaponBase.h"
+#include "CoreGameplayTags.h"
+#include "IrisInventoryComponent.h"
+#include "Inventory/Items/IrisInventoryFragment_WeaponBallistics.h"
+#include "IrisInventory/Public/IrisEquipmentInstance.h"
+
+UCC_WeaponFire_Hitscan::UCC_WeaponFire_Hitscan()
+{
+	//TODO Продолжить отсюда
+}
+
+void UCC_WeaponFire_Hitscan::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	if (!CachedBallistics || !CachedWeaponActor.IsValid())
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true,false);
+		return;
+	}
+	
+	//Коммитим кулдаун и трату патронов (GameplayEffect Cost/Cooldown)
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo)) {EndAbility(Handle, ActorInfo, ActivationInfo, true,false); return;}
+	
+	//0(1) Доступ к статам
+	int32 Pellets = CachedBallistics->PelletsPerShot;
+	FTransform MuzzleTransform = CachedWeaponActor->GetMuzzleTransform();
+	
+	//Триггерим клиентский визуал (GameplayCue)
+	//GAS автоматически выполнит это предсказание на клиенте и авторитарно на сервере
+	FGameplayCueParameters CueParams;
+	CueParams.Location = MuzzleTransform.GetLocation();
+	ActorInfo->AbilitySystemComponent->ExecuteGameplayCue(CachedBallistics->FireCueTag,CueParams);
+	
+	PerformFireMath(CachedWeaponActor.Get(),CachedBallistics);
+	
+	EndAbility(Handle, ActorInfo, ActivationInfo, true,false);
+}
+
+void UCC_WeaponFire_Hitscan::PerformFireMath(ACC_WeaponBase* WeaponActor,
+	const UIrisInventoryFragment_WeaponBallistics* Ballistics)
+{
+	FTransform MuzzleTransform = WeaponActor->GetMuzzleTransform();
+	FVector StartLoc = MuzzleTransform.GetLocation();
+	FVector ForwardDir = MuzzleTransform.GetRotation().GetForwardVector();
+	
+	for (int i = 0; i < Ballistics->PelletsPerShot; i++)
+	{
+		//Применяем Spread
+		FVector TraceDir = FMath::VRandCone(ForwardDir,FMath::DegreesToRadians(Ballistics->SpreadAngle));
+		FVector EndLoc = StartLoc + (TraceDir * Ballistics->MaxRange);
+		
+		FHitResult Hit;
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActor(GetAvatarActorFromActorInfo());
+		
+		if (GetWorld()->LineTraceSingleByChannel(Hit,StartLoc,EndLoc,ECC_Visibility,QueryParams))
+		{
+			//TODO Логика урона
+		}
+	}
+}
+
+void UCC_WeaponFire_Hitscan::OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnAvatarSet(ActorInfo, Spec);
+	
+	//Достаем SourceObject
+	if (UIrisEquipmentInstance* EquipInstance = Cast<UIrisEquipmentInstance>(Spec.SourceObject))
+	{
+		//Кешируем актор оружия (Если он заспавнен)
+		CachedWeaponActor = Cast<ACC_WeaponBase>(EquipInstance->SpawnedActor);
+		
+		//Кешируем тяжелый поиск фрагмента (Выполняется 1 раз за всю жизнь оружия)
+		if (const UIrisInventoryItemDefinition* ItemDef = EquipInstance->GetItemDef())
+		{
+			CachedBallistics = ItemDef->FindFragmentByClass<UIrisInventoryFragment_WeaponBallistics>();
+		}
+		
+		if (UIrisInventoryComponent* InventoryComp = ActorInfo->AvatarActor->FindComponentByClass<UIrisInventoryComponent>())
+		{
+			CachedInventoryComponent = InventoryComp;
+		}
+	}
+}
+
+bool UCC_WeaponFire_Hitscan::CheckCost(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, FGameplayTagContainer* OptionalRelevantTags) const
+{
+	//Оставляем базовую логику GAS (вдруг есть какие-то глобальные дебафы на стрельбу)
+	const bool bBaseCostMet = Super::CheckCost(Handle, ActorInfo, OptionalRelevantTags);
+	return bBaseCostMet;
+	if (!bBaseCostMet) return false;
+	
+	if (!CachedInventoryComponent) return false;
+	
+	int32 CurrentWeaponSlotIndex = GetEquippedWeaponSlotIndex();
+	
+	const int32 CurrentAmmo = CachedInventoryComponent->GetItemStat(CurrentWeaponSlotIndex,CoreGameplayTags::InventoryTags::Item_Stat_Ammo_Current);
+	
+	return CurrentAmmo > 0;
+}
+
+void UCC_WeaponFire_Hitscan::ApplyCost(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const
+{
+	Super::ApplyCost(Handle, ActorInfo, ActivationInfo);
+	
+	if (!CachedInventoryComponent) return;
+	
+	int32 CurrentWeaponSlotIndex = GetEquippedWeaponSlotIndex();
+	
+	CachedInventoryComponent->ModifyItemStat(CurrentWeaponSlotIndex,CoreGameplayTags::InventoryTags::Item_Stat_Ammo_Current,-1);
+	
+}
---
+++ Plugins/CombatCoreGASIris/Source/CombatCoreGASIris/Public/CC_WeaponFire_Hitscan.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "CC_WeaponFire_Hitscan.generated.h"
+
+class ACC_WeaponBase;
+class UIrisInventoryFragment_WeaponBallistics;
+class UIrisInventoryComponent;
+/**
+ * 
+ */
+UCLASS()
+class COMBATCOREGASIRIS_API UCC_WeaponFire_Hitscan : public UGameplayAbility
+{
+	GENERATED_BODY()
+public:
+	UCC_WeaponFire_Hitscan();
+	
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	
+	virtual void PerformFireMath(ACC_WeaponBase* WeaponActor,const UIrisInventoryFragment_WeaponBallistics* Ballistics);
+	virtual void OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	
+	virtual bool CheckCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+	virtual void ApplyCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const override;
+	
+	const UIrisInventoryFragment_WeaponBallistics* CachedBallistics = nullptr;
+	TWeakObjectPtr<ACC_WeaponBase> CachedWeaponActor = nullptr;
+	const UIrisInventoryComponent* CachedInventoryComponent = nullptr;
+};
