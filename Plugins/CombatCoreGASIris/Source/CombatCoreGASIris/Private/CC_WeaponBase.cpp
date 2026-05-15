24a68561c7555f5ea9fc862d0ce3460ec463bd46
Vladislav <vladislavprokopevcplusplus@gmail.com>
2026-05-15 15:19:49 +0500

    CC_WeaponBase

---
+++ Plugins/CombatCoreGASIris/Source/CombatCoreGASIris/Private/CC_WeaponBase.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+#include "CC_WeaponBase.h"
+
+
+ACC_WeaponBase::ACC_WeaponBase()
+{
+	PrimaryActorTick.bStartWithTickEnabled = false;
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+FTransform ACC_WeaponBase::GetMuzzleTransform() const
+{
+	return WeaponMesh->GetSocketTransform(MuzzleSocketName);
+}
+
+void ACC_WeaponBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
---
+++ Plugins/CombatCoreGASIris/Source/CombatCoreGASIris/Public/CC_WeaponBase.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Inventory/Items/IrisInventoryItemDefinition.h"
+#include "CC_WeaponBase.generated.h"
+
+UCLASS()
+class COMBATCOREGASIRIS_API ACC_WeaponBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ACC_WeaponBase();
+	
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Patameters")
+	FName MuzzleSocketName;
+	
+	UPROPERTY(Transient,BlueprintReadOnly,Category="Patameters")
+	TObjectPtr<const UIrisInventoryItemDefinition> WeaponDef;
+	
+	FTransform GetMuzzleTransform() const;
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(VisibleDefaultsOnly,BlueprintReadOnly,Category="Components")
+	TObjectPtr<USkeletalMeshComponent> WeaponMesh;
+};
