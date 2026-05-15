611c20d86867a894f98949f421929b2e112beb71
Vladislav <vladislavprokopevcplusplus@gmail.com>
2026-05-15 15:19:31 +0500

    BaseWeapon

---
+++ Source/PetProject/Private/Pickups/BaseWeapon.cpp
@@ -0,0 +1,20 @@
+﻿
+
+
+#include "Pickups/BaseWeapon.h"
+
+
+ABaseWeapon::ABaseWeapon()
+{
+	PrimaryActorTick.bStartWithTickEnabled = false;
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ABaseWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	
+}
+
+
---
+++ Source/PetProject/Public/Pickups/BaseWeapon.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseWeapon.generated.h"
+
+class UIrisInventoryItemDefinition;
+
+UCLASS()
+class PETPROJECT_API ABaseWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ABaseWeapon();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditDefaultsOnly)
+	UIrisInventoryItemDefinition* IrisInventoryItemDefinition;
+};
